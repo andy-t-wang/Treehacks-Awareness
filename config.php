280ccc419400b3351360f66a1d633b7aa900d541
Andy Wang <andywang@Andys-MacBook-Pro-2.local>
2019-02-16 14:32:49 -0800

    initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// ERROR REPORTING
+error_reporting(E_ALL & ~E_NOTICE);
+
+// DATABASE SETTINGS
+define('DB_HOST', 'verum.database.windows.net');
+define('DB_NAME', 'Verum');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'andywang@verum');
+define('DB_PASSWORD', 'Lightpower1');
+?>
